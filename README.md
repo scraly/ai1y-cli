2235383d3896fda75f476fcb194f28fb140b4d67
scraly <scraly@gmail.com>
2021-03-02 20:43:29 +0100

    feat: textToSpeach command on the CLI

--- README.md
+++ README.md
@@ -2,16 +2,45 @@
 
 ## CLI
 
-$ my-cli tr en
-$ my-cli text-to-speech fr
+```
+$ ai1y-cli textToSpeech hello en-US
+$ ai1y-cli textToSpeech salut fr-FR
+```
 
-github.com/scraly/go-text-to-speach
+## Run
 
+```
 $ go run main.go
+
+Usage:
+  ai1y-cli [command]
+
+Available Commands:
+  help         Help about any command
+  textToSpeech A brief description of your command
+
+Flags:
+      --config string   config file (default is $HOME/.ai1y-cli.yaml)
+  -h, --help            help for ai1y-cli
+  -t, --toggle          Help message for toggle
+
+Use "ai1y-cli [command] --help" for more information about a command.
+
+$ go run main.go tts
+textToSpeech called with text = Hello, World! and lang = en-US
 Audio content written to file: output.mp3
 
-## build program
-mkdir $ go build -o bin/text-to-speach main.go
+$ go run main.go tts "hello scraly"
+textToSpeech called with text = hello scraly and lang = en-US
+Audio content written to file: output.mp3
 
-execute:
-$ ./bin/text-to-speach
+$ go run main.go tts "salut scraly" fr-FR
+textToSpeech called with text = salut scraly and lang = fr-FR
+Audio content written to file: output.mp3
+```
+
+## Build program
+
+```
+$ go build -o bin/text-to-speach main.go
+````
--- cmd/textToSpeech.go
+++ cmd/textToSpeech.go
@@ -19,33 +19,91 @@ import (
 	"fmt"
 
 	"github.com/spf13/cobra"
+
+	"context"
+    "io/ioutil"
+	"log"
+
+    texttospeech "cloud.google.com/go/texttospeech/apiv1"
+    texttospeechpb "google.golang.org/genproto/googleapis/cloud/texttospeech/v1"
 )
 
 // textToSpeechCmd represents the textToSpeech command
 var textToSpeechCmd = &cobra.Command{
 	Use:   "textToSpeech",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Aliases: []string{"tts"},
+	Short: "Transform text to speech and export result in a mp3 file",
+	Long: `Usage: textToSpeech <text> <lang>`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("textToSpeech called")
+		var text = "Hello, World!"
+		var lang = "en-US"
+
+		if len(args) >= 1 && args[0] != "" {
+			text = args[0]
+		}
+
+		if len(args) == 2 && args[1] != "" {
+			lang = args[1]
+		}
+		fmt.Println("textToSpeech called with text =",text,"and lang =",lang)
+
+		textToSpeach(text, lang)
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(textToSpeechCmd)
+	// textToSpeechCmd.Flags().String("text", "", "Text to transform")
 
 	// Here you will define your flags and configuration settings.
 
 	// Cobra supports Persistent Flags which will work for this command
 	// and all subcommands, e.g.:
-	// textToSpeechCmd.PersistentFlags().String("foo", "", "A help for foo")
+	textToSpeechCmd.PersistentFlags().String("text", "", "Text to transform")
 
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
 	// textToSpeechCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
+
+func textToSpeach(text, lang string) {
+        ctx := context.Background()
+
+        client, err := texttospeech.NewClient(ctx)
+        if err != nil {
+                log.Fatal(err)
+        }
+
+        // Perform the text-to-speech request on the text input with the selected
+        // voice parameters and audio file type.
+        req := texttospeechpb.SynthesizeSpeechRequest{
+                // Set the text input to be synthesized.
+                Input: &texttospeechpb.SynthesisInput{
+                        InputSource: &texttospeechpb.SynthesisInput_Text{Text: text},
+                },
+                // Build the voice request, select the language code ("en-US") and the SSML
+                // voice gender ("neutral").
+                Voice: &texttospeechpb.VoiceSelectionParams{
+                        LanguageCode: lang,
+                        SsmlGender:   texttospeechpb.SsmlVoiceGender_NEUTRAL,
+                },
+                // Select the type of audio file you want returned.
+                AudioConfig: &texttospeechpb.AudioConfig{
+                        AudioEncoding: texttospeechpb.AudioEncoding_MP3,
+                },
+        }
+
+        resp, err := client.SynthesizeSpeech(ctx, &req)
+        if err != nil {
+                log.Fatal(err)
+        }
+
+        // The resp's AudioContent is binary.
+        filename := "output.mp3"
+        err = ioutil.WriteFile(filename, resp.AudioContent, 0644)
+        if err != nil {
+                log.Fatal(err)
+        }
+        fmt.Printf("Audio content written to file: %v\n", filename)
+}
+
